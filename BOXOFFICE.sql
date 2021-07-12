69caf02699aa69a036b366cc28ee1c6ddeb2a119
WolverineRast4 <87312386+WolverineRast4@users.noreply.github.com>
2021-07-12 09:23:29 -0300

    Add files via upload

---
+++ BOXOFFICE.sql
@@ -0,0 +1,4 @@
+USE pixar;
+
+INSERT INTO boxoffice (movie_id, rating, domestic_sales, internacional_sales)
+VALUES (8, 6.8, 4200000, 37000000);
---
+++ Insert  into pixar ruan info09.sql
@@ -0,0 +1,7 @@
+USE pixar;
+
+INSERT INTO movies(title, director, year, length_minutes)
+VALUES ('Monstro SA', 'Peter Docter', 2001, 92),
+	('Procurando Nemo', 'John Lasseter', 2003, 107),
+    ('Os Incríveis', 'Brad Bird', 2004, 116),
+    ('Wall-e', 'Peter Docter', 2008, 104);
